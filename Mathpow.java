4b163c9f071c11de9874185b3720d91b09e197c8
Thanush07RAW <draken0067@gmail.com>
2025-01-29 22:00:35 +0530

    Add files via upload

---
+++ Mathpow.java
@@ -0,0 +1,7 @@
+public class Mathpow{
+    public static void main(String[] args){
+        double n = 5, p = 7;
+        double res = Math.pow(n, p);
+        System.out.printf("%d to the power of %d is %d",(int) n, (int) p, (int) res);
+    }
+}
